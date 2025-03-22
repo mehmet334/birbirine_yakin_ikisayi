0c15f57ddd8072a2a7d0f00e9101dfb401eabb1f
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-22 17:48:30 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
+import java.util.Random;
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    // 10 adet benzersiz ve pozitif içeren listeyi random olarak belirleme
+
+        Set<Integer> sayilar = new HashSet<>();
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            while (sayilar.size()<10){
+                int sayi = rand.nextInt(1000);
+                sayilar.add(sayi);
+            }
         }
+        //En yakın iki sayıyı bulma
+        List<Integer> sayiliste = new ArrayList<>(sayilar);
+        Collections.sort(sayiliste);
+
+        int mindeger = Integer.MAX_VALUE;
+        int num1 = 0, num2 = 0;
+        for (int i = 0; i < 9; i++) {
+            int fark=sayiliste.get(i+1)-sayiliste.get(i);
+            if (fark<mindeger){
+                mindeger=fark;
+                num1=sayiliste.get(i);
+                num2=sayiliste.get(i+1);
+            }
+
+        }
+        System.out.println("Belirlenen sayılar: " + sayiliste);
+        System.out.println("En yakın iki sayı: "+ num1+ " ve " + num2);
     }
 }
